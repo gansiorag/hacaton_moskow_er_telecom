f09b8629d1a5f168bc98fb129a77415db228effc
gansior-ag <gansior-ag@rosseti.digital>
2021-05-27 14:04:03 +0300

    html path

--- get_information/get_info_restoraunt_two.py
+++ get_information/get_info_restoraunt_two.py
@@ -1,26 +1,47 @@
+"""
+This module get all info about restaurants from https://www.afisha.ru/msk/restaurants/restaurant_list/?view=list
+
+author Gansior A.G. gansior@gansior.ru
+"""
+
 import pprint
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 from drivers.init_drevers import init_drivers
 import time
+
 def get_all_href(driver):
-    rezult_file=open('href_restoraunts_1.csv', 'a')
-    driver.get('https://www.restoran.ru/msk/catalog/restaurants/all/')
-    my_key = True
-    kol = 0
-    while my_key :
-       kol +=1
-       print(kol)
-       all_page_a = driver.find_elements_by_xpath('//div[@class="place-about"]/a')
+    rezult_file=open('../href_restoraunts_2.csv', 'a')
+    driver.get('https://www.afisha.ru/msk/restaurants/restaurant_list/')
+    kol_all = 0
+    kol_tec = 1
+    while True:
+       print('kol_all ---> ', kol_all)
+       kol_all = kol_tec
+       butt = driver.find_element_by_xpath('//div[@class="show-more_button j-show-more-button"]/a')
+       #butt = driver.find_element_by_class_name("show-more_button j-show-more-button")
+       # bott = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, '//div[@class="show-more_button j-show-more-button"]/a')))
+       butt.click()
+       driver.implicitly_wait(20)
+       #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+       #time.sleep(6)
+       all_page_a = driver.find_elements_by_xpath('//li[@class="list_item"]')
+       kol_tec = len(all_page_a)
        print(len(all_page_a))
-       for href in all_page_a[0::2]:
-           print(href.text, href.get_attribute('href'), sep=' --> ')
-           rezult_file.write(href.text +';' +  href.get_attribute('href') + '\n')
-       try:
-           butt = driver.find_element_by_xpath('//li[@class="next-arrow"]/a')
-           butt.click()
-           time.sleep(1)
-       except Exception:
-           my_key = False
+       print('kol_tec ---> ', kol_tec)
+       # for href in all_page_a[0::2]:
+       #     print(href.text, href.get_attribute('href'), sep=' --> ')
+       #     rezult_file.write(href.text +';' +  href.get_attribute('href') + '\n')
+       # try:
+       #     butt = driver.find_element_by_xpath('//li[@class="next-arrow"]/a')
+       #     butt.click()
+       #     time.sleep(1)
+       # except Exception:
+       #     my_key = False
 
 def usulay_page(driver,all_rest, is_list):
     driver.find_element_by_xpath('//div[@class="read-more all-props-trigger-wrap"]').click()
@@ -51,33 +72,22 @@ def usulay_page(driver,all_rest, is_list):
         value = div.find_element_by_xpath('./div[@class="value"]').text
         all_rest[is_list[0]][name] = value
 
-def restoraut_net(driver, all_rest, is_list):
-    # level_one =driver.find_element_by_xpath('//div[@class="item active"]')
-    # level_two = driver.find_elements_by_xpath('//div[@class="contact props"]')
-    # params = base.find_elements_by_xpath('./div[@class="prop"]')
-    # all_rest[is_list[0]] = {}
-    pass
-
-
 
 def get_all_param(driver):
     all_rest ={}
     k=0
-    with open('href_restoraunts_1.csv', 'r') as is_f:
+    with open('../href_restoraunts_1.csv', 'r') as is_f:
         for line in is_f:
             k +=1
             print(k)
             if k>742:
                 is_list = line.strip().split(';')
                 driver.get(is_list[1])
-                try:
-                    usulay_page(driver, all_rest, is_list)
-                except Exception:
-                    restoraut_net(driver, all_rest, is_list)
+                usulay_page(driver, all_rest, is_list)
                 pprint.pprint(all_rest)
 
 
 if __name__ == '__main__':
-    driver = init_drivers()[0]
+    driver = init_drivers(vis=True)[0]
     get_all_href(driver)
     #get_all_param(driver)
