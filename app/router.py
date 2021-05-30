0123358554158622b4f4e1c272f7dbbbf38c6880
Alexander Gansior <gansior-ag@rosseti.digital>
2021-05-30 09:35:34 +0300

    create Flask local server

--- app/router.py
+++ app/router.py
@@ -6,13 +6,19 @@ Author Gansior A. mail - gansior@gansior.ru tel - +79173383804
 from app import app
 from flask import render_template
 
-@app.route("/")
-def index():
+@app.route("/map")
+def map():
     pcc = 37.620000
     lcc = 55.752900
     zoom = 5
     level = 100
     return render_template('/map_index.html',lcc = lcc, pcc = pcc, zoom = 13, level = level )
+
+@app.route("/")
+def index():
+    title = "Первая страница"
+    return render_template('/index.html', title = title)
+
 
 
 
