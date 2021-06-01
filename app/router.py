d92924571c665adc8d3038c9707028770f5063be
Alexander Gansior <gansior-ag@rosseti.digital>
2021-06-01 06:16:33 +0300

    create Flask local server

--- app/router.py
+++ app/router.py
@@ -10,9 +10,30 @@ from flask import render_template
 def map():
     pcc = 37.620000
     lcc = 55.752900
-    zoom = 5
+    zoom = 13
     level = 100
-    return render_template('/map_index.html',lcc = lcc, pcc = pcc, zoom = 13, level = level )
+    start_width = 55.7744
+    start_long = 37.580
+    end_width = 55.7294
+    end_long = 37.66
+    leng_side = 100
+
+    # def get_array_borders_squere(hi_point=[55.7744, 37.580], low_point=[55.7294, 37.66], leng_side=100):
+    #     width = abs(hi_point[0] - low_point[0])
+    #     long = abs(hi_point[1] - low_point[1])
+    #     print(width)
+    #     print(long)
+    #     kol_sqrt = int(width / 0.001)
+    #     koef_lend = leng_side / 100.0
+    #     print(kol_sqrt)
+
+    return render_template('/map_index.html',lcc = lcc, pcc = pcc, zoom = zoom, level = level,
+                            start_width = start_width,
+                            start_long = start_long,
+                            end_width = end_width,
+                            end_long = end_long,
+                            leng_side = leng_side
+                            )
 
 @app.route("/")
 def index():
--- app/servis_modules.py
+++ app/servis_modules.py
@@ -1,16 +1,36 @@
 """
-This module make
+This module collection of scripts to help
 
 Author Gansior A. mail - gansior@gansior.ru tel - +79173383804
+
 """
 from geopy import distance
+import math
 
 def proga():
     print(round(distance.GeodesicDistance([55.755,37.60176], [55.755,37.600]).m,0))
+
+def get_array_borders_squere(hi_point = [55.7744, 37.580],  low_point = [55.7294, 37.66], leng_side = 100):
+    width = abs(hi_point[0] - low_point[0])
+    long = abs(hi_point[1] - low_point[1])
+    print(width)
+    print(long)
+    kol_sqrt = int(width/0.001)
+    koef_lend = leng_side/100.0
+    print(kol_sqrt)
+    for ww in range(0, kol_sqrt):
+        for ll in range(0, kol_sqrt):
+            print(round(distance.GeodesicDistance([hi_point[0]+0.0009*ww*koef_lend,
+                                                   hi_point[1]+0.0016*ll*koef_lend],
+                                                   [hi_point[0] + 0.0009 * (ww + 1)*koef_lend,
+                                                    hi_point[1] + 0.0016 * (ll+1)*koef_lend]).m, 0))
+
+
 
 
 
 
 if __name__ == '__main__':
     proga()
+    get_array_borders_squere()
 
