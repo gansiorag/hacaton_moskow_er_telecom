c6dc88f5995d006f641ee70417a367c609382570
Alexander Gansior <gansior-ag@rosseti.digital>
2021-06-07 06:36:14 +0300

    work with datasets

---
+++ app/ETL_data_set/analiz_data_set_restoraunt.py
@@ -0,0 +1,13 @@
+"""
+This module make analysis data about restaurants
+data sets - 1. /home/al/PycharmProjects/hacaton_moskow_er_telecom/app/data_set/all_rest.csv
+
+Author Gansior A. mail - gansior@gansior.ru tel - +79173383804
+"""
+
+def proga():
+    pass
+
+
+if __name__ == '__main__':
+    proga()
