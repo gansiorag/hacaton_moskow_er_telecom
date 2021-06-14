13f5d69777abf6b45a6f22a2101791f561dbe26e
Pedro1810 <vbanochke@mail.ru>
2021-06-14 13:09:45 +0300

    addded pages

--- app/router.py
+++ app/router.py
@@ -54,8 +54,22 @@ def hist():
     return render_template('/special_data.html', title=title)
 
 
-@app.route("/valuation_by_address")
+@app.route("/valuation_by_address",methods=['GET', 'POST'])
 def valuation_by_address():
+    list_obj = []
+    if request.method == 'POST':
+        list_obj = request.form.getlist('type_obj')
+        print(list_obj)
+
+        if len(list_obj) > 0:
+            for objj in session['type_obj']:
+                if objj in list_obj:
+                    session['type_obj'][objj][1] = 'checked'
+                else:
+                    session['type_obj'][objj][1] = ''
+        else:
+            for objj in session['type_obj']: session['type_obj'][objj][1] = ''
+
     start_width = 55.754311
     start_long = 37.522732
     end_width = 55.701605
@@ -71,14 +85,15 @@ def valuation_by_address():
     lcc = (start_width + low_point[0]) / 2
 
     param = {'coord_pix': [[pcc, lcc], hi_point, low_point]}
-    name_obj = ['theaters', 'food', 'intercepting_parking', 'paid_parking', 'closed_paid_parking',
-                'cinemas', 'circus', 'concert_halls', 'museums', 'education']
     main_reactange = One_pix(param)
     kol_sqrt_width, kol_sqrt_long, koef_lend, array_sqrt = get_array_borders_squere(hi_point=[start_width, start_long],
                                                                                     low_point=[end_width, end_long],
                                                                                     leng_side=leng_side)
     print('kol_sqrt_width, kol_sqrt_long = ', kol_sqrt_width, kol_sqrt_long)
     work_array = Work_with_One_pix(main_reactange.array_objects_pix)
+    print(list_obj)
+    rezult = work_array.divide_data_sqrt(array_sqrt, list_obj)
+    print(rezult)
 
     address = request.values.get('ADDRESS')
 
@@ -93,11 +108,27 @@ def valuation_by_address():
                            leng_side=leng_side,
                            kol_sqrt_width=kol_sqrt_width,
                            kol_sqrt_long=kol_sqrt_long,
-                           koef_lend=koef_lend
+                           koef_lend=koef_lend,
+                           names_obj=session['type_obj'],
+                           see_obj=rezult
                            )
 
-@app.route("/valuation_by_coords")
+@app.route("/valuation_by_coords",methods=['GET', 'POST'])
 def valuation_by_coords():
+    list_obj = []
+    if request.method == 'POST':
+        list_obj = request.form.getlist('type_obj')
+        print(list_obj)
+
+        if len(list_obj) > 0:
+            for objj in session['type_obj']:
+                if objj in list_obj:
+                    session['type_obj'][objj][1] = 'checked'
+                else:
+                    session['type_obj'][objj][1] = ''
+        else:
+            for objj in session['type_obj']: session['type_obj'][objj][1] = ''
+
     start_width = 55.754311
     start_long = 37.522732
     end_width = 55.701605
@@ -106,21 +137,22 @@ def valuation_by_coords():
     low_point = [end_width, end_long]
     leng_side = 1250
     low_point = get_end_coord_rectangle(hi_point, low_point, leng_side)
-    zoom = 11 # start zoom
+    zoom = 11  # start zoom
 
     # this coordinates center rectangle
     pcc = (start_long + low_point[1]) / 2
     lcc = (start_width + low_point[0]) / 2
 
     param = {'coord_pix': [[pcc, lcc], hi_point, low_point]}
-    name_obj = ['theaters', 'food', 'intercepting_parking', 'paid_parking', 'closed_paid_parking',
-                'cinemas', 'circus', 'concert_halls', 'museums', 'education']
     main_reactange = One_pix(param)
     kol_sqrt_width, kol_sqrt_long, koef_lend, array_sqrt = get_array_borders_squere(hi_point=[start_width, start_long],
                                                                                     low_point=[end_width, end_long],
                                                                                     leng_side=leng_side)
     print('kol_sqrt_width, kol_sqrt_long = ', kol_sqrt_width, kol_sqrt_long)
     work_array = Work_with_One_pix(main_reactange.array_objects_pix)
+    print(list_obj)
+    rezult = work_array.divide_data_sqrt(array_sqrt, list_obj)
+    print(rezult)
 
     coords = request.values.get('COORDS')
 
@@ -135,7 +167,9 @@ def valuation_by_coords():
                            leng_side=leng_side,
                            kol_sqrt_width=kol_sqrt_width,
                            kol_sqrt_long=kol_sqrt_long,
-                           koef_lend=koef_lend
+                           koef_lend=koef_lend,
+                           names_obj=session['type_obj'],
+                           see_obj=rezult
                            )
 
 @app.route("/search_by_heat_map",methods=['GET', 'POST'])
